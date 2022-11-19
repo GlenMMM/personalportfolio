0923cbcb623eb148de48fc0ab21cbb18c2b6fe32
jeremy <jpwijanto@gmail.com>
2022-11-19 21:59:10 +0700

    add og and twitter meta tags

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,17 @@ function Header({ ...props }) {
       <meta name="description" content={props.description} />
       <link rel="icon" href="/glenicon.png" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://glenmarvin.com/" />
+      <meta property="og:title" content={props.title} />
+      <meta property="og:description" content={props.description} />
       <meta property="og:image" content="/glenicon.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://glenmarvin.com/" />
+      <meta property="twitter:title" content={props.title} />
+      <meta property="twitter:description" content={props.description} />
+      <meta property="twitter:image" content="/glenicon.png" />
     </Head>
   );
 }
